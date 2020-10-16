b91e105d581702257b81aecb1b3a2cdbbdb6c05e
zeroing2 <zeroing2@gmail.com>
2020-10-17 08:50:29 +0900

    [bug_fix] 테스트 복구

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -15,8 +15,10 @@ import org.springframework.context.annotation.FilterType;
 )
 public class AutoAppConfig {
 
+    /*
     @Bean(name = "memoryMemberRepository")
     MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
+     */
 }
