3a91f43fd10d97185578057b639544008cb9340d
zeroing2 <zeroing2@gmail.com>
2020-10-17 10:03:59 +0900

    [changed_minor][의존관계 자동 주입] 롬복과 최신 트랜드

---
+++ src/main/java/hello/core/HelloLombok.java
@@ -0,0 +1,25 @@
+package hello.core;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+//@NoArgsConstructor
+public class HelloLombok {
+
+    private String name;
+    private int age;
+
+    public static void main(String[] args) {
+        HelloLombok helloLombok = new HelloLombok();
+        helloLombok.setName("zeroing2");
+
+        String name = helloLombok.getName();
+        System.out.println("name = " + name);
+        System.out.println("HelloLombok = " + helloLombok);
+    }
+}
